a0a67ef31d14aaabd3ee4e897bf756e20dfd2515
yamamoto-masafumi <10th.ten@gmail.com>
2021-03-25 11:54:34 +0900

    voiceのレスポンシブ対応

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,13 @@
 $(function(){
   $('#voice_left_text_more').on('click', function(){
     $('#voice_left_text_more_area').slideToggle();
+    $('#voice_left_text_more_close').removeClass('hidden').addClass('active');
+    $(this).removeClass('activce').addClass('hidden');
+  });
+
+  $('#voice_left_text_more_close').on('click', function(){
+    $('#voice_left_text_more_area').slideToggle();
+    $('#voice_left_text_more').removeClass('hidden').addClass('active');
+    $(this).removeClass('activce').addClass('hidden');
   });
 });
